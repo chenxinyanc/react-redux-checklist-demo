ea5452c16dbee190ec6f1e77b20dd9c748233ea4
Xinyan Chen <xinychen@microsoft.com>
2019-11-13 19:57:18 +0800

    Add item editor.

---
+++ src/components/CheckListItem.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames';
+import moment from 'moment';
+import { CommandBarButton, Stack, TextField, DatePicker } from 'office-ui-fabric-react';
+import * as React from 'react';
+import { CheckListItemState } from '../state/types';
+import classes from './CheckListItem.scss';
+
+export interface CheckListItemProps {
+    item: CheckListItemState
+    onAcceptChanges: (change: Readonly<Partial<CheckListItemState> & Pick<CheckListItemState, 'id'>>) => void;
+    viewOperationArea?: React.ReactNode;
+}
+
+export const CheckListItem: React.FC<CheckListItemProps> = (props) => {
+    const { item } = props;
+    const [isEditing, setIsEditing] = React.useState(false);
+    const [edits, setEdits] = React.useState<Partial<CheckListItemState>>({});
+    return (<Stack className={classes.checkListItem} key={item.id} horizontal data-is-focusable>
+        {
+            isEditing
+                ? (
+                    <React.Fragment>
+                        <Stack.Item grow>
+                            <TextField
+                                value={edits.title ?? item.title}
+                                onChange={(e, n) => setEdits({ ...edits, title: n })}
+                                label="Title"
+                            />
+                        </Stack.Item>
+                        <DatePicker
+                            label="Due"
+                            value={edits.due ?? item.due}
+                            onSelectDate={(d) => d && setEdits({ ...edits, due: d })}
+                        />
+                        <CommandBarButton
+                            iconProps={{ iconName: 'CheckMark' }}
+                            disabled={Object.keys(edits).length === 0}
+                            onClick={() => {
+                                props.onAcceptChanges({ ...edits, id: item.id });
+                                setIsEditing(false);
+                            }} />
+                        <CommandBarButton iconProps={{ iconName: 'Cancel' }} onClick={() => setIsEditing(false)} />
+                    </React.Fragment>
+                )
+                : (<React.Fragment>
+                    <Stack grow>
+                        <div className={classes.title}>{item.title}</div>
+                        <div className={classNames(classes.due, item.due == null && classes.noDue)}>{item.due == null ? 'No due' : moment(item.due).format()}</div>
+                    </Stack>
+                    {props.viewOperationArea}
+                    <CommandBarButton iconProps={{ iconName: 'Edit' }} onClick={() => setIsEditing(true)} />
+                </React.Fragment>
+                )
+        }
+    </Stack>);
+}
+CheckListItem.displayName = 'CheckListItem';
--- src/containers/CheckList.tsx
+++ src/containers/CheckList.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { CheckListItemState, CheckListState } from '../state/types';
 import { List, FocusZone } from 'office-ui-fabric-react';
-import { connect } from 'react-redux';
-import classes from './CheckList.scss';
-import moment = require('moment');
+import { connect, MapDispatchToProps } from 'react-redux';
+import { CheckListItem } from '../components/CheckListItem';
+import { CheckListAction, editItem } from '../state/actions';
+import { Dispatch } from 'redux';
 
 export interface CheckListOwnProps {
 
@@ -13,26 +14,34 @@ interface CheckListStateProps {
     items: CheckListItemState[];
 }
 
+interface CheckListDispatchProps {
+    onEditItem: (change: Readonly<Partial<CheckListItemState> & Pick<CheckListItemState, 'id'>>) => void;
+}
+
+
 function mapStateToProps(state: CheckListState): CheckListStateProps {
     return { items: state.items };
 }
 
-function onRenderItem(item?: CheckListItemState, index?: number): React.ReactNode {
-    if (!item) {
-        return null;
+function mapDispatchToProps(dispatch: Dispatch<CheckListAction>): CheckListDispatchProps {
+    return {
+        onEditItem(change) { dispatch(editItem(change)); }
     }
-    return (<div className={classes.checkListItem} key={item.id} data-is-focusable>
-        <div className={classes.title}>{item.title}</div>
-        <div className={classes.due}>{item.due == null ? 'No due' : moment(item.due).format()}</div>
-    </div>);
 }
 
-export class CheckListBase extends React.PureComponent<CheckListOwnProps & CheckListStateProps> {
+export class CheckListBase extends React.PureComponent<CheckListOwnProps & CheckListStateProps & CheckListDispatchProps> {
+    private _onRenderItem = (item?: CheckListItemState, index?: number): React.ReactNode => {
+        if (!item) {
+            return null;
+        }
+        return <CheckListItem key={index} item={item} onAcceptChanges={this.props.onEditItem} />
+    }
+
     public render() {
         return (<FocusZone>
-            <List<CheckListItemState> items={this.props.items} onRenderCell={onRenderItem} />
+            <List<CheckListItemState> items={this.props.items} onRenderCell={this._onRenderItem} />
         </FocusZone>);
     }
 }
 
-export const CheckList = connect(mapStateToProps)(CheckListBase);
+export const CheckList = connect(mapStateToProps, mapDispatchToProps)(CheckListBase);
